f49ceb9b30002401f3b3219cd1e40d9a685a4a44
Yuanxuan Wang <zellux@gmail.com>
2012-10-25 21:20:25 +0800

    Check number of letters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   end
 
   def search
+    if params[:letters].scan(/\w/).size != 25
+      flash[:error] = I18n.t('error.25_letters')
+      redirect_to(root_path)
+    end
     @letters = params[:letters].upcase.scan(/\w/).each_slice(5)
     @list = Solver.search(params[:letters])
   end
