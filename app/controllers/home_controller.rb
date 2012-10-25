13c162ff090a7b1e6a6c8fb2cfafe518d5a1bd9b
Yuanxuan Wang <zellux@gmail.com>
2012-10-25 20:40:06 +0800

    Show letter matrix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   end
 
   def search
+    @letters = params[:letters].upcase.scan(/\w/).each_slice(5)
     @list = Solver.search(params[:letters])
   end
 end
