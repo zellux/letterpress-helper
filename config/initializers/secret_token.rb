97c3ab48983fb816c8c0187c402994522c556b95
Yuanxuan Wang <zellux@gmail.com>
2012-10-25 12:38:02 +0800

    Initial commit with slim and bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetterpressHelper::Application.config.secret_token = 'b08b8a4e1a6e130767a6902d889503d103f883d61832f7838aba9f0c1a763ff08bc83866a0983374e58f57fe2f2f2127776295c058cf42fade369a83622e457f'
